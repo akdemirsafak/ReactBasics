39e9b6edbb5520f86c706f01f4cde921a9e329de
akdemirsafak <akdemirsafak@gmail.com>
2024-01-16 23:13:40 +0300

    added basics

--- js-objects/src/App.js
+++ js-objects/src/App.js
@@ -1,35 +1,20 @@
 import React,{ useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
     
-  const[ friends, setFriends ]= useState([
-    {id:1,name:"Angejia",age:25,gender:"fm"},
-    {id:2,name:"zack",age:19,gender:"m"},
-    {id:3,name:"mirella",age:22,gender:"fm"},
-    {id:4,name:"Patrick",age:27,gender:"m"}
-    ]
-  ); 
+  const[ user, setUser ]= useState({}); 
 
-  const clickHandle=()=>{
-    const someFriends=friends.filter((friend)=>friend.gender==="fm");
-
-    // setFriends(friends.filter(friend=>friend.age<=19)); //tek değer gelse de bu değer bir array içerisinde gelir.
-    // console.log(friends.filter(friend=>friend.age<=19));
-    //console.log(someFriends);
-
-
-    //setFriends([...someFriends]);  
-    setFriends([...friends.filter((friend)=>friend.gender==="fm"), {id:123,name:"joe",age:15,gender:"m"}]);  //Burada yeni dizi oluşturmadan filtreleme yaptık. 3 noktanın önemi burada ortaya çıkıyor.Aslında bir dizi var fakat bir isimle tanımlaması yapılmadı.
-    setFriends([...someFriends, {id:123,name:"joe",age:15,gender:"m"}]);
-  };
 
   return (
     <div className="App">
-    <button onClick={()=>clickHandle()}>Tıkla</button>
-    {
-      friends && friends.map((friend)=>{
-        return <h4 key={friend.id} >Name : {friend.name} | Age {friend.age} | Gender : {friend.gender}</h4>      
-      })
-    }
+      <main className='container my-5'>
+      <input type='text' placeholder='Name'  className='d-block mb-2' onChange={(e)=>setUser({...user,name:e.target.value})}/>
+      <input type='text' placeholder='LastName' className='d-block mb-2' onChange={(e)=>setUser({...user,lastname:e.target.value})} />
+      <input type='number' placeholder='Age'  className='d-block mb-2'  onChange={(e)=>setUser({...user,age:e.target.value})}/>
+
+      <h3>{user.name} {user.lastname} {user.age}</h3>
+      </main>
     </div>
   );
 }
