3b4f07086297e2760ebc008e10c95e4c9013096e
akdemirsafak <akdemirsafak@gmail.com>
2024-01-16 22:20:05 +0300

    Filters added.

--- js-objects/src/App.js
+++ js-objects/src/App.js
@@ -1,40 +1,34 @@
 import React,{ useState } from 'react';
 function App() {
     
-  const[students,setStudents]= useState([]); //useState() içerisinde bir değer vermemizin sebebi null check yapmak istemeemizden dolayı.
+  const[ friends, setFriends ]= useState([
+    {id:1,name:"Angejia",age:25,gender:"fm"},
+    {id:2,name:"zack",age:19,gender:"m"},
+    {id:3,name:"mirella",age:22,gender:"fm"},
+    {id:4,name:"Patrick",age:27,gender:"m"}
+    ]
+  ); 
 
   const clickHandle=()=>{
+    const someFriends=friends.filter((friend)=>friend.gender==="fm");
 
-    const friends=[];
-    friends.push({id:1,name:"anna",age:25,gender:"fm"})
-    friends.push({id:2,name:"zack",age:19,gender:"m"})
-    friends.push({id:3,name:"mirella",age:22,gender:"fm"})
-    friends.push({id:4,name:"Patrick",age:27,gender:"m"});
-
-
-    setStudents(friends);
-
-    //console.log(students);
-
-    for (let index = 0; index < friends.length; index++) {
-      console.log(friends[index]);
-    }
+    // setFriends(friends.filter(friend=>friend.age<=19)); //tek değer gelse de bu değer bir array içerisinde gelir.
+    // console.log(friends.filter(friend=>friend.age<=19));
+    //console.log(someFriends);
 
 
-    };
+    //setFriends([...someFriends]);  
+    setFriends([...friends.filter((friend)=>friend.gender==="fm"), {id:123,name:"joe",age:15,gender:"m"}]);  //Burada yeni dizi oluşturmadan filtreleme yaptık. 3 noktanın önemi burada ortaya çıkıyor.Aslında bir dizi var fakat bir isimle tanımlaması yapılmadı.
+    setFriends([...someFriends, {id:123,name:"joe",age:15,gender:"m"}]);
+  };
+
   return (
     <div className="App">
     <button onClick={()=>clickHandle()}>Tıkla</button>
     {
-      students.map((student)=>{
-        return <h4 key={student.id} >Name : {student.name} | Age {student.age} | Gender : {student.gender}</h4>
-      
-        //eğer state'in başlangıç değeri olmasaydı aşağıdaki kullanım yapılabilirdi.:
-        // students && students.map((student)=>{
-        // return <h4 key={student.id} >Name : {student.name} | Age {student.age} | Gender : {student.gender}</h4>
-      
+      friends && friends.map((friend)=>{
+        return <h4 key={friend.id} >Name : {friend.name} | Age {friend.age} | Gender : {friend.gender}</h4>      
       })
-
     }
     </div>
   );
