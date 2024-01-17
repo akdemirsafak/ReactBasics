428de804b16ee07d73fa159e60765e372e59206a
akdemirsafak <akdemirsafak@gmail.com>
2024-01-17 14:32:34 +0300

    formdan datalar alındı.

--- js-objects/src/App.js
+++ js-objects/src/App.js
@@ -5,17 +5,58 @@ function App() {
     
   const[ user, setUser ]= useState({}); 
 
+  const[users,setUsers]=useState([]);
+
+  const changeHandler = (e)=>{
+    setUser({...user, [e.target.name] : e.target.value});
+  }
+  const clickHandler = ()=>{
+    setUsers([...users,user]);
+    setUser({});
+  }
 
   return (
-    <div className="App">
+    <>
       <main className='container my-5'>
-      <input type='text' placeholder='Name'  className='d-block mb-2' onChange={(e)=>setUser({...user,name:e.target.value})}/>
-      <input type='text' placeholder='LastName' className='d-block mb-2' onChange={(e)=>setUser({...user,lastname:e.target.value})} />
-      <input type='number' placeholder='Age'  className='d-block mb-2'  onChange={(e)=>setUser({...user,age:e.target.value})}/>
 
-      <h3>{user.name} {user.lastname} {user.age}</h3>
+      <input type='text' placeholder='Name' name='Name'  className='d-block mb-2' onChange={(e)=>changeHandler(e)}/>
+      <input type='text' placeholder='LastName' name='LastName' className='d-block mb-2' onChange={(e)=>changeHandler(e)} />
+      <input type='number' placeholder='Age' name='Age' className='d-block mb-2'  onChange={(e)=>changeHandler(e)}/>
+      
+      <h3>{user.Name} {user.LastName} {user.Age}</h3>
+
+      <button onClick={()=>clickHandler()}> Ekle </button>
+      {
+        // Eğer users daha önce tanımlanmadıysa undefined'dır. Undefined'ın property'si olmaz. users && users.length şeklinde kullanabiliriz.
+        users.length===0 ? <h3>Öğrenci yok.</h3> :
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>LastName</th>
+              <th>Age</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              users.map((user,index) => 
+              {
+                return (
+                  <tr key={index}>
+                    <td>{user.Name}</td>
+                    <td>{user.LastName}</td>
+                    <td>{user.Age}</td>
+                  </tr>
+                )
+              })
+            } 
+          </tbody>
+
+        </table>
+      }
+      
       </main>
-    </div>
+    </>
   );
 }
 
